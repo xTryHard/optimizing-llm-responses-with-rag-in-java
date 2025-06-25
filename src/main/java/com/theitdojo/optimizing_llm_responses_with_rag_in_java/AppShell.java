a644ce90bab752d8775d1b3aa963d13eee06cca6
Jose Rafael Almonte Cabrera <jose.cabrera@franklincovey.com>
2025-06-25 14:11:20 -0400

    chore: Move Vaadin server-push enablement to application entry point

--- src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/AppShell.java
+++
@@ -1,8 +0,0 @@
-package com.theitdojo.optimizing_llm_responses_with_rag_in_java;
-
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.component.page.Push;
-
-@Push
-public class AppShell implements AppShellConfigurator {
-}
--- src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/OptimizingLlmResponsesWithRagInJavaApplication.java
+++ src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/OptimizingLlmResponsesWithRagInJavaApplication.java
@@ -1,10 +1,13 @@
 package com.theitdojo.optimizing_llm_responses_with_rag_in_java;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Push
 @SpringBootApplication
-public class OptimizingLlmResponsesWithRagInJavaApplication {
+public class OptimizingLlmResponsesWithRagInJavaApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OptimizingLlmResponsesWithRagInJavaApplication.class, args);
