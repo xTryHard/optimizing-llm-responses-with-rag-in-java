64902bfc98ce9ac3f30cdd883f053a3ab80a4d72
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-07-18 15:31:47 -0400

    creating /chat endpoint to manage user prompt and return a string response based on the response stream

--- src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/controllers/ChatController.java
+++ src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/controllers/ChatController.java
@@ -1,8 +1,12 @@
 package com.theitdojo.optimizing_llm_responses_with_rag_in_java.controllers;
 
+import java.util.stream.Stream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.theitdojo.optimizing_llm_responses_with_rag_in_java.services.ChatAssistant;
@@ -15,5 +19,18 @@ public class ChatController {
 
     public ChatController(ChatAssistant chatAssistant) {
         this.chatAssistant = chatAssistant;
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String message, @RequestParam(defaultValue = "false") boolean stream) {
+        Stream<String> response = chatAssistant.askQuestion(message, stream);
+        StringBuilder responseBuilder = new StringBuilder();
+
+        response.forEach(chunk -> {
+            logger.info(chunk);
+            responseBuilder.append(chunk);
+        });
+
+        return responseBuilder.toString();
     }
 }
